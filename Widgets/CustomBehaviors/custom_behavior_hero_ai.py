1f81cf763b21a84d46e3656e930434901a887002
Mark <markpnoi26@gmail.com>
2025-07-24 05:13:22 +0100

    added base heroAI integration

--- Widgets/CustomBehaviors/custom_behavior_hero_ai.py
+++ Widgets/CustomBehaviors/custom_behavior_hero_ai.py
@@ -7,8 +7,8 @@ def execute_enhanced_custom_heroai_combat():
 
     custom_combat_behavior = CustomBehaviorLoader().custom_combat_behavior
 
-    if custom_combat_behavior is not None:
-        CustomBehaviorLoader().ensure_custom_behavior_match_in_game_build()
+    if custom_combat_behavior and not custom_combat_behavior.is_custom_behavior_match_in_game_build():
+        CustomBehaviorLoader().refresh_custom_behavior_candidate()
 
     if custom_combat_behavior:
         custom_combat_behavior.hero_ai_execute()
--- Widgets/CustomBehaviors/primitives/skillbars/custom_behavior_base_utility.py
+++ Widgets/CustomBehaviors/primitives/skillbars/custom_behavior_base_utility.py
@@ -1,20 +1,24 @@
 from abc import abstractmethod
 from collections import deque
-from typing import List, Generator, Any, override
+from typing import Any
+from typing import Generator
 
-from HeroAI.cache_data import CacheData
-from Py4GWCoreLib import GLOBAL_CACHE, Routines, Range
+from Py4GWCoreLib import GLOBAL_CACHE
+from Py4GWCoreLib import Routines
 from Widgets.CustomBehaviors.primitives.behavior_state import BehaviorState
 from Widgets.CustomBehaviors.primitives.helpers import custom_behavior_helpers
 from Widgets.CustomBehaviors.primitives.parties.custom_behavior_party import CustomBehaviorParty
-from Widgets.CustomBehaviors.primitives.skillbars.custom_behavior_skillbar_management import CustomBehaviorSkillbarManagement
+from Widgets.CustomBehaviors.primitives.skillbars.custom_behavior_skillbar_management import (
+    CustomBehaviorSkillbarManagement,
+)
 from Widgets.CustomBehaviors.primitives.skills.custom_skill import CustomSkill
 from Widgets.CustomBehaviors.primitives.skills.custom_skill_utility_base import CustomSkillUtilityBase
 from Widgets.CustomBehaviors.skills.generic.hero_ai_utility import HeroAiUtility
 from Widgets.CustomBehaviors.primitives.scores.score_static_definition import ScoreStaticDefinition
 from Widgets.CustomBehaviors.primitives.constants import DEBUG
 
-class CustomBehaviorBaseUtility():
+
+class CustomBehaviorBaseUtility:
     """
     This class serves as a blueprint for creating custom combat behaviors that
     are compatible with specific game builds. Subclasses implementing this class
@@ -26,11 +30,10 @@ class CustomBehaviorBaseUtility():
     # priority behavior if few mana
     # do-not-overlap mode => with shared-memory-lock / or shared-memory-queue
 
-
     def __init__(self):
         super().__init__()
         self._generator_handle = self._handle()
-        self.__is_enabled:bool = False
+        self.__is_enabled: bool = False
         self.__previously_attempted_skills: deque[CustomSkill] = deque(maxlen=40)
         self.skillbar_management: CustomBehaviorSkillbarManagement = CustomBehaviorSkillbarManagement()
         self.__final_skills_list: list[CustomSkillUtilityBase] | None = None
@@ -47,17 +50,17 @@ class CustomBehaviorBaseUtility():
         return self._fetch_state()
 
     def get_final_state(self) -> BehaviorState:
-        party_forced_state:BehaviorState|None = CustomBehaviorParty().get_party_forced_state()
+        party_forced_state: BehaviorState | None = CustomBehaviorParty().get_party_forced_state()
         account_state = self.get_state()
-        final_state:BehaviorState = account_state if party_forced_state is None else party_forced_state
+        final_state: BehaviorState = account_state if party_forced_state is None else party_forced_state
         return final_state
 
     def get_is_enabled(self) -> bool:
         return self.__is_enabled
 
     def get_final_is_enabled(self) -> bool:
-        party_forced_state:bool = CustomBehaviorParty().get_party_is_enable()
-        final_is_enabled:bool = party_forced_state and self.__is_enabled
+        party_forced_state: bool = CustomBehaviorParty().get_party_is_enable()
+        final_is_enabled: bool = party_forced_state and self.__is_enabled
         return final_is_enabled
 
     # abstract/overridable
@@ -86,7 +89,7 @@ class CustomBehaviorBaseUtility():
     def skills_allowed_in_behavior(self) -> list[CustomSkillUtilityBase]:
         '''
         the list of skills that are allowed in the behavior.
-        if a skill is not in this list, 2 options : 
+        if a skill is not in this list, 2 options :
             - if complete_build_with_generic_skills is True, the behavior will complete the build with generic skills.
             - if False, the behavior will not use the skill at all.
         '''
@@ -100,21 +103,21 @@ class CustomBehaviorBaseUtility():
         '''
         pass
 
-    #build management
+    # build management
 
     def count_matches_between_custom_behavior_and_in_game_build(self) -> int:
-            '''
-            count the number of skills in the custom behavior (skills_required_in_behavior) that are in the in-game build.
-            '''
-            result:int = 0
-            in_game_build: dict[int, CustomSkill] = self.skillbar_management.get_in_game_build()
-            custom_behavior_build: list[CustomSkill] = self.skills_required_in_behavior
+        '''
+        count the number of skills in the custom behavior (skills_required_in_behavior) that are in the in-game build.
+        '''
+        result: int = 0
+        in_game_build: dict[int, CustomSkill] = self.skillbar_management.get_in_game_build()
+        custom_behavior_build: list[CustomSkill] = self.skills_required_in_behavior
 
-            for custom_skill in custom_behavior_build:
-                if in_game_build.get(custom_skill.skill_id) is not None:
-                    result +=1
+        for custom_skill in custom_behavior_build:
+            if in_game_build.get(custom_skill.skill_id) is not None:
+                result += 1
 
-            return result
+        return result
 
     def get_skills_final_list(self) -> list[CustomSkillUtilityBase]:
         '''
@@ -126,20 +129,30 @@ class CustomBehaviorBaseUtility():
 
         if self.__final_skills_list is not None:
             return self.__final_skills_list
-        
+
         in_game_build_by_skill_id: dict[int, CustomSkill] = self.skillbar_management.get_in_game_build()
-        skills_allowed_in_behavior_by_skill_id: dict[int, CustomSkillUtilityBase] = {x.custom_skill.skill_id: x for x in self.skills_allowed_in_behavior}
+        skills_allowed_in_behavior_by_skill_id: dict[int, CustomSkillUtilityBase] = {
+            x.custom_skill.skill_id: x for x in self.skills_allowed_in_behavior
+        }
         # skills_required_in_behavior: list[CustomSkill] = self.skills_required_in_behavior
         final_list: list[CustomSkillUtilityBase] = []
 
         for skill in in_game_build_by_skill_id.values():
-            if skill is None: raise ValueError(f"Skill is None")
-            if skill.skill_id == 0: raise ValueError(f"Skill {skill.skill_id} is not in the build")
-            
+            if skill is None:
+                raise ValueError("Skill is None")
+            if skill.skill_id == 0:
+                raise ValueError(f"Skill {skill.skill_id} is not in the build")
+
             if skill.skill_id in skills_allowed_in_behavior_by_skill_id.keys():
                 final_list.append(skills_allowed_in_behavior_by_skill_id[skill.skill_id])
             elif self.complete_build_with_generic_skills:
-                final_list.append(HeroAiUtility(skill=skill, current_build=list(in_game_build_by_skill_id.values()), score_definition=ScoreStaticDefinition(0)))
+                final_list.append(
+                    HeroAiUtility(
+                        skill=skill,
+                        current_build=list(in_game_build_by_skill_id.values()),
+                        score_definition=ScoreStaticDefinition(0),
+                    )
+                )
 
         for skill in self.additional_autonomous_skills:
             final_list.append(skill)
@@ -148,18 +161,22 @@ class CustomBehaviorBaseUtility():
         return self.__final_skills_list
 
     def is_custom_behavior_match_in_game_build(self) -> bool:
-        if not GLOBAL_CACHE.Map.IsOutpost(): return True
+        if not GLOBAL_CACHE.Map.IsOutpost():
+            return True
 
-        utility_build_full:list[CustomSkillUtilityBase] = self.get_skills_final_list()
-        is_completed:bool = self.complete_build_with_generic_skills
-        in_game_build:dict[int, CustomSkill] = self.skillbar_management.get_in_game_build()
+        utility_build_full: list[CustomSkillUtilityBase] = self.get_skills_final_list()
+        is_completed: bool = self.complete_build_with_generic_skills
+        in_game_build: dict[int, CustomSkill] = self.skillbar_management.get_in_game_build()
 
         # check if ingame slots match our definitions
         for skill in utility_build_full:
-            if skill.custom_skill.skill_id != 0: #meaning it's an autonomous skill
+            if skill.custom_skill.skill_id != 0:  # meaning it's an autonomous skill
                 skill_id = GLOBAL_CACHE.SkillBar.GetSkillIDBySlot(skill.custom_skill.skill_slot)
                 if skill_id != skill.custom_skill.skill_id:
-                    if DEBUG: print(f"Slot {skill.custom_skill.skill_slot} doesn't match skill {skill.custom_skill.skill_id}, the behavior must be refreshed.")
+                    if DEBUG:
+                        print(
+                            f"Slot {skill.custom_skill.skill_slot} doesn't match skill {skill.custom_skill.skill_id}, the behavior must be refreshed."
+                        )
                     return False
 
         # two case
@@ -168,22 +185,30 @@ class CustomBehaviorBaseUtility():
             # check if all ingame skills are in the behavior.
             for skill_id in in_game_build.keys():
                 if skill_id not in [item.custom_skill.skill_id for item in utility_build_full]:
-                    if DEBUG: print(f"{skill_id} from in-game build doesn't exist in the behavior, the behavior must be refreshed.")
+                    if DEBUG:
+                        print(
+                            f"{skill_id} from in-game build doesn't exist in the behavior, the behavior must be refreshed."
+                        )
                     return False
 
         if not is_completed:
             #  1/ check if all skills in the behavior are part of the in-game build.
             for skill in utility_build_full:
-                if skill.custom_skill.skill_id == 0: continue
+                if skill.custom_skill.skill_id == 0:
+                    continue
                 if skill.custom_skill.skill_id not in in_game_build.keys():
-                    if DEBUG: print(f"{skill.custom_skill.skill_id} that is present in the behavior is not part of the in-game build, the behavior must be refreshed.")
+                    if DEBUG:
+                        print(
+                            f"{skill.custom_skill.skill_id} that is present in the behavior is not part of the in-game build, the behavior must be refreshed."
+                        )
                     return False
 
             #  2/ check if we added a new ingame skill that should be part of the behavior.
             for skill_id in in_game_build.keys():
                 if skill_id not in [item.custom_skill.skill_id for item in utility_build_full]:
                     if skill_id in [item.custom_skill.skill_id for item in self.skills_allowed_in_behavior]:
-                        if DEBUG: print(f"{skill_id} should be present in the behavior, the behavior must be refreshed.")
+                        if DEBUG:
+                            print(f"{skill_id} should be present in the behavior, the behavior must be refreshed.")
                         return False
 
         return True
@@ -192,9 +217,11 @@ class CustomBehaviorBaseUtility():
 
     def act(self):
 
-        if not self.get_final_is_enabled(): return
-        if not Routines.Checks.Map.MapValid(): return
-        if not self.is_custom_behavior_match_in_game_build(): 
+        if not self.get_final_is_enabled():
+            return
+        if not Routines.Checks.Map.MapValid():
+            return
+        if not self.is_custom_behavior_match_in_game_build():
             print("Custom behavior doesn't match in game build, you are not allowed to perform behavior.act().")
             return
 
@@ -206,7 +233,7 @@ class CustomBehaviorBaseUtility():
                 hero_ai_options.Following = hero_ai_options.Following
                 hero_ai_options.Looting = hero_ai_options.Looting
 
-        final_state:BehaviorState = self.get_final_state()
+        final_state: BehaviorState = self.get_final_state()
 
         if final_state == BehaviorState.IDLE:
             return
@@ -214,13 +241,34 @@ class CustomBehaviorBaseUtility():
             try:
                 next(self._generator_handle)
             except StopIteration:
-                print(f"act is not expected to StopIteration.")
+                print("act is not expected to StopIteration.")
             except Exception as e:
                 print(f"act is not expected to exit : {e}")
 
+    def hero_ai_execute(self):
+        if not self.get_final_is_enabled():
+            return
+        if not Routines.Checks.Map.MapValid():
+            return
+        if not self.is_custom_behavior_match_in_game_build():
+            print("Custom behavior doesn't match in game build, you are not allowed to perform behavior.act().")
+            return
+
+        final_state: BehaviorState = self.get_final_state()
+
+        if final_state == BehaviorState.IDLE:
+            return
+        else:
+            try:
+                next(self._generator_handle)
+            except StopIteration:
+                print(f"hero_ai_execute.{BehaviorState(final_state)} is not expected to StopIteration.")
+            except Exception as e:
+                print(f"hero_ai_execute.{BehaviorState(final_state)} is not expected to exit : {e}")
+
     def _fetch_state(self) -> BehaviorState:
 
-        if self.get_final_is_enabled() == False:
+        if not self.get_final_is_enabled():
             return BehaviorState.IDLE
 
         if not Routines.Checks.Map.MapValid():
@@ -245,24 +293,24 @@ class CustomBehaviorBaseUtility():
         utilities: list[CustomSkillUtilityBase] = self.get_skills_final_list()
         # for x in utilities:
         #     print(f"skill {x.custom_skill.skill_name}")
-        
+
         utility_scores: list[tuple[CustomSkillUtilityBase, float | None]] = []
         for utility in utilities:
             score = utility.evaluate(self.get_final_state(), list(self.__previously_attempted_skills))
             utility_scores.append((utility, score))
-        
+
         # Sort by score (highest first)
         utility_scores.sort(key=lambda x: x[1] if x[1] is not None else 0, reverse=True)
         return utility_scores
 
     def _handle(self) -> Generator[Any | None, Any | None, None]:
-        
+
         while True:
-            
+
             utility_scores: list[tuple[CustomSkillUtilityBase, float | None]] = self.get_all_scores()
 
             # #take highest scoring
-            highest_scoring : tuple[CustomSkillUtilityBase, float | None] = utility_scores[0]
+            highest_scoring: tuple[CustomSkillUtilityBase, float | None] = utility_scores[0]
             if highest_scoring[1] is None:
                 yield
                 continue
